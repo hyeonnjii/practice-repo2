6ab68f5881b90f29c032c0640fdb279fb06027e3
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2024-01-12 17:37:47 +0900

    refactor: Create main.py
    To hyeonnjii.py make run on main function
    But, I already make it prior blob running on main
    This is Just practice using prefix

--- hyeonnjii.py
+++ hyeonnjii.py
@@ -3,6 +3,8 @@ def adder_twoNum(num1:int, num2:int) -> int:
 
 if __name__ == '__main__':
     get_nums = input("Enter the integer numbers(using seperator ','): ").replace(' ', '').split(',')
+    
+    # strip은 인자 1개를 주로 사용, replace 는 여러개로 사용가능!
     num1, num2 = map(int, get_nums)
     print(add_twoNum(num1, num2))
 
